ee2934842279a52f5c8de7ccb8b81c096609dda8
Jeremy Kendall <jeremy@jeremykendall.net>
2013-12-16 14:02:53 -0600

    Uncrufts bootstrap.php

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -5,10 +5,8 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 date_default_timezone_set('America/Chicago');
 
-define('APPLICATION_PATH', realpath(__DIR__ . '/..'));
-
-$loader = require APPLICATION_PATH . '/vendor/autoload.php';
-$loader->add('JeremyKendall\\Slim\\Auth\\Tests\\', APPLICATION_PATH . '/tests');
+$loader = require dirname(__DIR__) . '/vendor/autoload.php';
+$loader->add('JeremyKendall\\Slim\\Auth\\Tests\\', __DIR__);
 
 define('SLIM_MODE', 'testing');
 

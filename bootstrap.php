f3af698bbf60f423cf18bcbb4ffbef29e5f75d53
Jeremy Kendall <jeremy@jeremykendall.net>
2013-12-16 09:23:38 -0600

    Reorgs code

--- bootstrap.php
+++ bootstrap.php
@@ -5,10 +5,9 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 date_default_timezone_set('America/Chicago');
 
-define('APPLICATION_PATH', realpath(__DIR__ . '/..'));
+define('APPLICATION_PATH', realpath(__DIR__));
 
-$loader = require APPLICATION_PATH . '/vendor/autoload.php';
-$loader->add('JeremyKendall\\Slim\\Auth\\Tests\\', APPLICATION_PATH . '/tests');
+$loader = require './vendor/autoload.php';
 
 define('SLIM_MODE', 'testing');
 
